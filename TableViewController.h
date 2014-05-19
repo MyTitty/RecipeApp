ca09aefa449fb96bdf18fae619e0a2f5e86c6069
horie naoto <ne230085@senshu-u.jp>
2014-05-19 16:54:06 +0900

    Create TableViewController.h

---
+++ TableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TableViewController.h
+//  lab04
+//
+//  Created by 堀江直人 on 2014/05/10.
+//  Copyright (c) 2014年 堀江直人. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UIViewController
+
+@property IBOutlet UITableView *ListTableView;
+
+@end
