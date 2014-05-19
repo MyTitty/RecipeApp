48d20578c9b3f64be2089cc71e1507e510d145f9
horie naoto <ne230085@senshu-u.jp>
2014-05-19 16:58:08 +0900

    Create Recipe.h

---
+++ Recipe.h
@@ -0,0 +1,15 @@
+//
+//  Recipe.h
+//  lab04
+//
+//  Created by 堀江直人 on 2014/05/17.
+//  Copyright (c) 2014年 堀江直人. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Recipe : NSObject
+
++(void)initialize;
+
+@end
